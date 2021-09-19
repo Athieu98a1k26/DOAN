fc7782fe08a0ab38366f9f71eb626aa1ae174ddb
Được trần <trantienduoc@gmail.com>
2021-09-19 11:56:59 +0700

    thongtindulich

---
+++ KMT.DATA_MODEL/ThongTinDuLich/ThongTinDuLichRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KMT.DATA_MODEL.ThongTinDuLich
+{
+    public class ThongTinDuLichRequest
+    {
+        public int Id { get; set; }
+        public string TIEUDE { get; set; }
+        public int? TRANGTHAI { get; set; }
+        public int page { get; set; }
+        public int take { get; set; }
+    }
+}
