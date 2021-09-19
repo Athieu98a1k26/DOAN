bc819c11ebccaf1d390bda1e1821a3cdc6ded90d
Được trần <trantienduoc@gmail.com>
2021-09-19 10:47:19 +0700

    update sp

---
+++ KMT.DATA_MODEL/SanPham/SanPhamRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KMT.DATA_MODEL.SanPham
+{
+    public class SanPhamRequest
+    {
+        public int Id { get; set; }
+        public bool IsDelete { get; set; }
+        public string TENMATHANG { get; set; }
+        public int? GIA { get; set; }
+        public int? GIAMIN { get; set; }
+        public int? GIAMAX { get; set; }
+        public int page { get; set; }
+        public int take { get; set; }
+    }
+}
