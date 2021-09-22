51d24ffa61db2a82fede2891b3abc83a6589fd69
Được trần <trantienduoc@gmail.com>
2021-09-23 00:35:48 +0700

    Đẩy full code

---
+++ KMT.WEB_FRONTEND/Controllers/ChiTietSanPhamController.cs
@@ -0,0 +1,25 @@
+﻿using KMT.DATA_MODEL.SanPham;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KMT.WEB_FRONTEND.Controllers
+{
+    public class ChiTietSanPhamController : BaseController
+    {
+        // GET: ChiTietSanPham
+        public async Task<ActionResult> Index(int? Id)
+        {
+            if (!Id.HasValue)
+            {
+                Response.Redirect("/");
+                Response.End();
+            }
+            SanPhamInfo data = await ApiService.sanPhamService.GetById(Id.Value);
+            return View(data);
+        }
+    }
+}
